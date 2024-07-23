23d85b0cfd3d37e75ca8b55bd3a245ac49beba77
hro19 <fourbasic19@gmail.com>
2024-07-23 21:54:29 +0900

    student一覧ページ表示せず

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Students</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($students as $student)
+                <tr>
+                    <td>{{ $student->id }}</td>
+                    <td>{{ $student->name }}</td>
+                    <td>{{ $student->email }}</td>
+                    <td>
+                        <a href="{{ route('students.show', $student) }}">View</a>
+                        <a href="{{ route('students.edit', $student) }}">Edit</a>
+                        <form action="{{ route('students.destroy', $student) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
