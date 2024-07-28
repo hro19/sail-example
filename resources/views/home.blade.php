82b6a172aadc82a73384ace352ce18f5061224de
hro19 <fourbasic19@gmail.com>
2024-07-28 18:02:57 +0900

    heroiconsを設置

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,10 @@
     <p class="p-3 bg-amber-600"><a href="{{route('recipes.index')}}" class="underline">recipesページ</a></p>
 
     <div class="mx-3">     
-        <h2 class="my-3 py-2 px-3 text-3xl border-4 border-yellow-600">レシピ最新</h2>
+        <h2 class="my-3 py-2 px-3 text-3xl border-4 border-yellow-600">
+          <svg class="inline-block w-9 mr-1 -mt-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 4.5v15m6-15v15m-10.875 0h15.75c.621 0 1.125-.504 1.125-1.125V5.625c0-.621-.504-1.125-1.125-1.125H4.125C3.504 4.5 3 5.004 3 5.625v12.75c0 .621.504 1.125 1.125 1.125Z" />
+          </svg>レシピ最新</h2>
         <p class="mb-3"><a href={{route('recipes.index')}} class="underline text-lg">全てのレシピ ></a></p>
         <section class="grid grid-cols-1 md:grid-cols-2 gap-4">
             @foreach ($recipes as $recipe)
@@ -21,7 +24,10 @@
 
     
     <div class="mx-3">     
-        <h2 class="my-3 py-2 px-3 text-3xl border-4 border-cyan-500">人気レシピ</h2>
+        <h2 class="my-3 py-2 px-3 text-3xl border-4 border-cyan-500"><svg class="inline-block w-9 mr-1 -mt-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3 3v1.5M3 21v-6m0 0 2.77-.693a9 9 0 0 1 6.208.682l.108.054a9 9 0 0 0 6.086.71l3.114-.732a48.524 48.524 0 0 1-.005-10.499l-3.11.732a9 9 0 0 1-6.085-.711l-.108-.054a9 9 0 0 0-6.208-.682L3 4.5M3 15V4.5" />
+          </svg>
+          人気レシピ</h2>
         <section class="grid grid-cols-1 md:grid-cols-2 gap-4">
             @foreach ($populars as $p)
                 <x-recipes.front-card :recipe="$p" />
