3d226bee18a8d00acbe5cb1a42a0bde75486ee73
hro19 <fourbasic19@gmail.com>
2024-08-10 01:59:20 +0900

    feat: Show preferences on student detail page

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -1,17 +1,42 @@
 <x-app-layout>
     <x-wrap.card title="学生詳細" content="学生の詳細情報です" />
-    <div class="m-3 text-3xl">
-        <p>ID: {{ $student->id }}</p>
-        <p>名前: {{ $student->name }}</p>
-        <p>メールアドレス: {{ $student->email }}</p>
-        {{-- 他の属性も必要に応じて表示 --}}
-
-        @foreach($student->preferences as $preference)
-        <article class="border-4 border-blue-500 p-3 ml-2">
-            <p>preferenceのID: {{ $preference->id }}</p>
-            <p>名前: {{ $preference->name }}</p>
-            <p>生成日: {{ $preference->created_at }}</p>
-        </article>
-        @endforeach
+    <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+        <div class="px-4 py-5 sm:px-6">
+            <h3 class="text-lg leading-6 font-medium text-gray-900">学生情報</h3>
+        </div>
+        <div class="border-t border-gray-200">
+            <dl>
+                <div class="bg-yellow-200 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-g-500">ID</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $student->id }}</dd>
+                </div>
+                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">名前</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $student->name }}</dd>
+                </div>
+                <div class="bg-yellow-200 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">メールアドレス</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $student->email }}</dd>
+                </div>
+                {{-- 他の属性も必要に応じて表示 --}}
+    
+                <div class="ml-4">
+                @foreach($student->preferences as $preference)
+                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">Preference の ID</dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $preference->id }}</dd>
+                    </div>
+                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">名前</dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $preference->name }}</dd>
+                    </div>
+                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">生成日</dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $preference->created_at }}</dd>
+                    </div>
+                @endforeach
+                </div>
+            </dl>
+        </div>
     </div>
 </x-app-layout>
